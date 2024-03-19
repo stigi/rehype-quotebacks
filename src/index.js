8faa505587e9319c1d1b2e177e22e5d7a5d80fd4
Ullrich Schäfer <hi+github@ullrich.is>
2024-03-19 06:36:12 +0100

    first working version

--- src/index.js
+++ src/index.js
@@ -1,3 +1,91 @@
-export default function remarkQuoteback() {
-  return 1 + 1
+import {visit} from 'unist-util-visit'
+
+const parseCite = node => {
+  // checking basic cite structure
+  if (
+    !node
+    || node.tagName !== 'p'
+    || node.children.length !== 3
+    || node.children[0].type !== 'text'
+    || node.children[0].value !== 'Source: '
+    || node.children[1].tagName !== 'a'
+    || node.children[2].type !== 'text'
+    || !node.children[2].value.startsWith(' by ')
+  ) {
+    return
+  }
+
+  return {
+    author: node.children[2].value.slice(4),
+    url: node.children[1].properties.href,
+    publication: node.children[1].children[0].value,
+  }
+}
+
+export default function rehypeQuotebacks() {
+  return function (tree) {
+    // console.log('tree', tree)
+    visit(tree, {tagName: 'blockquote'}, (node, index, parent) => {
+      if (!parent || !parent.children) {
+        return
+      }
+
+      const maybeCite = parseCite(parent.children[index + 2])
+      if (!maybeCite) {
+        return
+      }
+
+      parent.children.splice(index + 2, 1) // remove cite, it's info be added into the quoteback
+
+      // if cite is followed by a line break, remove it (happens when there's another block after the cite, like a code block or paragraph)
+      if (
+        parent.children[index + 2]
+        && parent.children[index + 2].type === 'text'
+        && parent.children[index + 2].value === '\n'
+      ) {
+        parent.children.splice(index + 2, 1)
+      }
+
+      // Found a blockquote with a cite. Let's turn it into a quoteback!
+
+      node.properties = {
+        className: ['quoteback'],
+        'data-title': maybeCite.publication,
+        'data-author': maybeCite.author,
+        cite: maybeCite.url,
+      }
+
+      // Adding the cite footer inside the blockquote
+      node.children = [...node.children, {
+        type: 'element',
+        tagName: 'footer',
+        children: [
+          {type: 'text', value: maybeCite.author},
+          {
+            type: 'element', tagName: 'cite', children: [
+              {type: 'text', value: ' '},
+              {
+                type: 'element', tagName: 'a', properties: {href: maybeCite.url}, children: [{type: 'text', value: maybeCite.publication}],
+              },
+            ],
+          },
+        ],
+      }, {type: 'text', value: '\n'}]
+
+      // Adding the quoteback script after the blockquote
+      parent.children.splice(
+        index + 1,
+        0,
+        {type: 'text', value: '\n'},
+        {
+          type: 'element',
+          tagName: 'script',
+          properties: {
+            note: '',
+            src: 'https://cdn.jsdelivr.net/gh/Blogger-Peer-Review/quotebacks@1/quoteback.js',
+          },
+        },
+      )
+    })
+  }
 }
--- test.js
+++ test.js
@@ -1,10 +1,69 @@
 import test from 'ava'
+import rehypeStringify from 'rehype-stringify'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import {unified} from 'unified'
+import rehypeQuotebacks from './src/index.js'
 
-test('foo', t => {
-  t.pass()
+const process = async markdown =>
+  String(
+    await unified()
+      .use(remarkParse)
+      .use(remarkRehype)
+      .use(rehypeQuotebacks)
+      .use(rehypeStringify).process(markdown),
+  )
+
+// Tests
+
+test('Processes one line quotes in isolation', async t => {
+  const markdown = `> Here's something smart I wrote so you can quote.
+
+Source: [My Blog](https://example.org/blog) by Anonymous Author
+`
+
+  t.snapshot(await process(markdown))
 })
 
-test('bar', async t => {
-  const bar = Promise.resolve('bar')
-  t.is(await bar, 'bar')
+test('Processes multiline quotes in isolation', async t => {
+  const markdown = `> Here's something smart I wrote so you can quote:
+>
+> Words flow with great ease,
+> Thoughts become wisdom in ink,
+> Smartness on the page.
+
+Source: [My Blog](https://example.org/blog) by Anonymous Author
+`
+
+  t.snapshot(await process(markdown))
+})
+
+test('Processes one line quotes in context', async t => {
+  const markdown = `# How to quoteback?
+
+> Here's something smart I wrote so you can quote.
+
+Source: [My Blog](https://example.org/blog) by Anonymous Author
+
+And this is how you quoteback.
+`
+
+  t.snapshot(await process(markdown))
+})
+
+test('Processes multiline quotes in context', async t => {
+  const markdown = `# How to quoteback?
+
+> Here's something smart I wrote so you can quote:
+>
+> Words flow with great ease,
+> Thoughts become wisdom in ink,
+> Smartness on the page.
+
+Source: [My Blog](https://example.org/blog) by Anonymous Author
+
+And this is how you quoteback.
+`
+
+  t.snapshot(await process(markdown))
 })
